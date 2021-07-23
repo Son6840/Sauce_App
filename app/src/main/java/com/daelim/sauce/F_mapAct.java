e20ec4ccffef1a71ded03efc03e608ab7ea522b4
Son <sangbae6840@gmail.com>
2021-07-23 19:09:38 +0900

    DefaultLocation 지정

--- app/src/main/java/com/daelim/sauce/F_mapAct.java
+++ app/src/main/java/com/daelim/sauce/F_mapAct.java
@@ -1,6 +1,9 @@
 package com.daelim.sauce;
 
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,24 +14,67 @@ import androidx.fragment.app.Fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.content.ContentValues.TAG;
+
 public class F_mapAct extends Fragment implements OnMapReadyCallback,GoogleMap.OnInfoWindowClickListener{
     private View view;
     private MapView googlemap = null;
+
+    private Marker currentMarker = null;
+    private static final String TAG = "googlemap_example";
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int UPDATE_INTERVAL_MS = 1000;  // 1초
+    private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
+
+    // onRequestPermissionsResult에서 수신된 결과에서 ActivityCompat.requestPermissions를 사용한 퍼미션 요청을 구별하기 위해 사용됩니다.
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    boolean needRequest = false;
+
+    // 앱을 실행하기 위해 필요한 퍼미션을 정의합니다.
+    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
+
+
+    Location mCurrentLocatiion;
+    LatLng currentPosition;
+
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationRequest;
+    private Location location;
 
     @Nullable
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_map, container, false);
+
+        locationRequest = new LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL_MS)
+                .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
+
+
+        LocationSettingsRequest.Builder builder =
+                new LocationSettingsRequest.Builder();
+
+        builder.addLocationRequest(locationRequest);
+
         //MapView 호출
         googlemap = (MapView) view.findViewById(R.id.map);
         googlemap.onCreate(savedInstanceState);
@@ -71,30 +117,38 @@ public class F_mapAct extends Fragment implements OnMapReadyCallback,GoogleMap.O
     //맵뷰 설정
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: ");
+        setDefaultLocation(googleMap);
+
+    }
+    public void setDefaultLocation(GoogleMap googleMap){
         //마커찍기(위도,경도)
-        LatLng Daelim = new LatLng(37.40387620799947, 126.93031303076037);
+        LatLng DEFAULT_LOCATION = new LatLng(37.40387620799947, 126.93031303076037);
 
         //마커 옵션
-        MarkerOptions marker = new MarkerOptions();
-        marker.position(Daelim); //마커 위치
-        marker.title("대림대학교");
-        marker.snippet("경기도 안양시 동안구 비산1동 임곡로 대림대학교");
 
-        //맵에 마커표시, 인포윈도우 보여줌
-        googleMap.addMarker(marker).showInfoWindow();
+        String markerTitle = "위치정보 가져올 수 없음";
+        String markerSnippet = "위치권한과 gps 활성여부 확인";
 
-        //인포윈도우 클릭
-        googleMap.setOnInfoWindowClickListener(this);
+        if (currentMarker != null) currentMarker.remove();
 
-        //맵뷰 카메라위치, 줌 설정
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Daelim, 13));
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(DEFAULT_LOCATION);
+        markerOptions.title(markerTitle);
+        markerOptions.snippet(markerSnippet);
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        currentMarker = googleMap.addMarker(markerOptions);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 15);
+        googleMap.moveCamera(cameraUpdate);
+
     }
-
     //인포윈도우 클릭 리스너
     @Override
     public void onInfoWindowClick (Marker marker){
 
-    }
+   }
 
 }
 
