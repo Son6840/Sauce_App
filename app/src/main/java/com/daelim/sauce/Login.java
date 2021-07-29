c16ebed990e80372b30b98d382eaddaf420d1b16
jdonghun <jdonghun45@gmail.com>
2021-07-30 08:51:43 +0900

    카카오 로그인 닉네임 화면
    오류로 인한 작업

--- app/src/main/java/com/daelim/sauce/Login.java
+++ app/src/main/java/com/daelim/sauce/Login.java
@@ -5,15 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.User;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
 
 public class Login extends AppCompatActivity {
+    private static final String TAG = "Login";
     EditText mID;
     EditText mPW;
-    Button login;
+    private View loginButton, logoutButton;
+    private TextView nickName;
+    private ImageView profileImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +37,36 @@ public class Login extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
+        loginButton = findViewById(R.id.login2);
+        logoutButton = findViewById(R.id.logout);
+        //nickName = findViewById(R.id.nickname);
+       // profileImage = findViewById(R.id.profile);
+
+      /*  Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                if (oAuthToken != null) {
+                    // TBD
+                }
+                if (throwable != null) {
+                    // TBD
+                }
+                updateKakaoLoginUi();
+                return null;
+            }
+        };*/
+
+
+
+
+
+
+
         updateKakaoLoginUi();
 
 
         mID = (EditText) findViewById(R.id.ID);
         mPW = (EditText) findViewById(R.id.PASSWORD);
-        login = (Button) findViewById(R.id.login);
 
         Button join = (Button) findViewById(R.id.Join);
         Button home = (Button) findViewById(R.id.home);
@@ -49,5 +89,31 @@ public class Login extends AppCompatActivity {
 
     }
 
+    private void updateKakaoLoginUi() {
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                if(user !=null){
+                    Log.i(TAG,"invoke: id=" + user.getId());
+                    Log.i(TAG,"invoke: email=" + user.getKakaoAccount().getEmail());
+                    Log.i(TAG,"invoke: nickname=" + user.getKakaoAccount().getProfile().getNickname());
+                    Log.i(TAG,"invoke: gender=" + user.getKakaoAccount().getGender());
+                    Log.i(TAG,"invoke: age=" + user.getKakaoAccount().getAgeRange());
+
+                    nickName.setText(user.getKakaoAccount().getProfile().getNickname());
+                   Glide.with(profileImage).load(user.getKakaoAccount().getProfile().getThumbnailImageUrl()).into(profileImage);
+                    loginButton.setVisibility(View.GONE);
+                    logoutButton.setVisibility(View.VISIBLE);
+                } else{
+                    nickName.setText(null);
+                    profileImage.setImageBitmap(null);
+                    logoutButton.setVisibility(View.VISIBLE);
+                    logoutButton.setVisibility(View.GONE);
+
+                }
+                return null;
+            }
+        });
+    }
 
 }
