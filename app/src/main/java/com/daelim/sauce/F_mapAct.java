23439480e1b9be02caed91c3dca7802417ccd856
Son <sangbae6840@gmail.com>
2021-08-01 22:45:46 +0900

    위치정보 permission 추가

--- app/src/main/java/com/daelim/sauce/F_mapAct.java
+++ app/src/main/java/com/daelim/sauce/F_mapAct.java
@@ -1,7 +1,11 @@
 package com.daelim.sauce;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,10 +14,13 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
@@ -32,48 +39,36 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import static android.content.ContentValues.TAG;
 
-public class F_mapAct extends Fragment implements OnMapReadyCallback,GoogleMap.OnInfoWindowClickListener{
-    private View view;
-    private MapView googlemap = null;
+public class F_mapAct extends Fragment implements OnMapReadyCallback,GoogleMap.OnInfoWindowClickListener, ActivityCompat.OnRequestPermissionsResultCallback{
+private View view;
+private MapView googlemap = null;
 
-    private Marker currentMarker = null;
-    private static final String TAG = "googlemap_example";
-    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
-    private static final int UPDATE_INTERVAL_MS = 1000;  // 1초
-    private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
+private Marker currentMarker = null;
+private static final String TAG = "googlemap_example";
+private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+private static final int UPDATE_INTERVAL_MS = 1000;  // 1초
+private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
 
-    // onRequestPermissionsResult에서 수신된 결과에서 ActivityCompat.requestPermissions를 사용한 퍼미션 요청을 구별하기 위해 사용됩니다.
-    private static final int PERMISSIONS_REQUEST_CODE = 100;
-    boolean needRequest = false;
+// onRequestPermissionsResult에서 수신된 결과에서 ActivityCompat.requestPermissions를 사용한 퍼미션 요청을 구별하기 위해 사용됩니다.
+private static final int PERMISSIONS_REQUEST_CODE = 100;
+        boolean needRequest = false;
 
-    // 앱을 실행하기 위해 필요한 퍼미션을 정의합니다.
-    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
-
-
-    Location mCurrentLocatiion;
-    LatLng currentPosition;
+        // 앱을 실행하기 위해 필요한 퍼미션을 정의합니다.
+        String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
 
 
-    private FusedLocationProviderClient mFusedLocationClient;
-    private LocationRequest locationRequest;
-    private Location location;
+        Location mCurrentLocatiion;
+        LatLng currentPosition;
 
-    @Nullable
+private FusedLocationProviderClient mFusedLocationClient;
+private LocationRequest locationRequest;
+private Location location;
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+@Override
+public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_map, container, false);
-
-        locationRequest = new LocationRequest()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(UPDATE_INTERVAL_MS)
-                .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
-
-
-        LocationSettingsRequest.Builder builder =
-                new LocationSettingsRequest.Builder();
-
-        builder.addLocationRequest(locationRequest);
 
         //MapView 호출
         googlemap = (MapView) view.findViewById(R.id.map);
@@ -81,47 +76,46 @@ public class F_mapAct extends Fragment implements OnMapReadyCallback,GoogleMap.O
         googlemap.getMapAsync(this);
 
         return view;
-    }
-    //이 메서드가 없으면 지도 출력 x
-    @Override
-    public void onStart() {
+        }
+//이 메서드가 없으면 지도 출력 x
+@Override
+public void onStart() {
         super.onStart();
         googlemap.onStart();
-    }
+        }
 
-    @Override
-    public void onStop () {
+@Override
+public void onStop () {
         super.onStop();
         googlemap.onStop();
 
-    }
-
-    @Override
-    public void onSaveInstanceState (@Nullable Bundle outState){
+        }
+@Override
+public void onSaveInstanceState (@Nullable Bundle outState){
         super.onSaveInstanceState(outState);
         googlemap.onSaveInstanceState(outState);
-    }
+        }
 
-    @Override
-    public void onResume() {
+@Override
+public void onResume() {
         super.onResume();
         googlemap.onResume();
-    }
+        }
 
-    @Override
-    public void onDestroy() {
+@Override
+public void onDestroy() {
         super.onDestroy();
         googlemap.onLowMemory();
-    }
+        }
 
-    //맵뷰 설정
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
+//맵뷰 설정
+@Override
+public void onMapReady(GoogleMap googleMap) {
         Log.d(TAG, "onMapReady: ");
         setDefaultLocation(googleMap);
 
-    }
-    public void setDefaultLocation(GoogleMap googleMap){
+        }
+public void setDefaultLocation(GoogleMap googleMap){
         //마커찍기(위도,경도)
         LatLng DEFAULT_LOCATION = new LatLng(37.40387620799947, 126.93031303076037);
 
@@ -143,12 +137,13 @@ public class F_mapAct extends Fragment implements OnMapReadyCallback,GoogleMap.O
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 15);
         googleMap.moveCamera(cameraUpdate);
 
-    }
-    //인포윈도우 클릭 리스너
-    @Override
-    public void onInfoWindowClick (Marker marker){
+        }
+//인포윈도우 클릭 리스너
+@Override
+public void onInfoWindowClick (Marker marker){
 
-   }
+        }
+
 
 }
 
