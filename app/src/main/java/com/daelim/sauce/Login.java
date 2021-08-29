7886b9ad020c98c0e890286008253856948e4325
jdonghun <jdonghun45@gmail.com>
2021-08-29 18:13:03 +0900

    middle

--- app/src/main/java/com/daelim/sauce/Login.java
+++ app/src/main/java/com/daelim/sauce/Login.java
@@ -21,8 +21,6 @@ import com.google.android.material.tabs.TabLayout;
 import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.User;
-import com.nhn.android.naverlogin.OAuthLogin;
-import com.nhn.android.naverlogin.OAuthLoginHandler;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
@@ -30,29 +28,85 @@ import kotlin.jvm.functions.Function2;
 
 public class Login extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
+    EditText mID;
+    EditText mPW;
     Button join;
     Button home;
+    Button owner;
     TextView nothing;
     private View loginButton, logoutButton;
     private TextView nickName;
     private ImageView profileImage;
-    private ImageView naverlogin;
-    OAuthLogin mOAuthLoginModule;
-    Context mContext;
+    String st;
+   // private ImageView imageView2;
 
+   // OAuthLogin mOAuthLoginModule;
+    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        mContext = getApplicationContext();
 
-        naverlogin = findViewById(R.id.imageView2);//naver login
+       // imageView2 = findViewById(R.id.imageView2);
         loginButton = findViewById(R.id.login);
         logoutButton = findViewById(R.id.logout);
         nickName = findViewById(R.id.nickname);
         profileImage = findViewById(R.id.profile);
         nothing = findViewById(R.id.nothing);
+
+       /* imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOAuthLoginModule = OAuthLogin.getInstance();
+                mOAuthLoginModule.init(
+                        mContext
+                        ,getString(R.string.naver_client_id)
+                        ,getString(R.string.naver_client_secret)
+                        ,getString(R.string.naver_client_name)
+                );
+                @SuppressLint("HandlerLeak")
+                OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+                    @Override
+                    public void run(boolean success) {
+                        if (success) {
+                            String accessToken = mOAuthLoginModule.getAccessToken(mContext);
+                            String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
+                            long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
+                            String tokenType = mOAuthLoginModule.getTokenType(mContext);
+
+                            Log.i("LoginData","accessToken : "+ accessToken);
+                            Log.i("LoginData","refreshToken : "+ refreshToken);
+                            Log.i("LoginData","expiresAt : "+ expiresAt);
+                            Log.i("LoginData","tokenType : "+ tokenType);
+
+                        } else {
+                            String errorCode = mOAuthLoginModule
+                                    .getLastErrorCode(mContext).getCode();
+                            String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
+                            Toast.makeText(mContext, "errorCode:" + errorCode
+                                    + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+                        }
+                    };
+                };
+
+                mOAuthLoginModule.startOauthLoginActivity(Login.this, mOAuthLoginHandler);
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOAuthLoginModule.logout(mContext);
+                Toast.makeText(Login.this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+*/
+
+
+
 
         Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
             @Override
@@ -67,46 +121,6 @@ public class Login extends AppCompatActivity {
                 return null;
             }
         };
-        naverlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mOAuthLoginModule = OAuthLogin.getInstance();
-                mOAuthLoginModule.init(
-                        mContext
-                        , getString(R.string.naver_client_id)
-                        , getString(R.string.naver_client_secret)
-                        , getString(R.string.naver_client_name)
-                );
-                @SuppressLint("HandlerLeak")
-                OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
-                    @Override
-                    public void run(boolean success) {
-                        if (success) {
-                            String accessToken = mOAuthLoginModule.getAccessToken(mContext);
-                            String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
-                            long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
-                            String tokenType = mOAuthLoginModule.getTokenType(mContext);
-
-                            Log.i("LoginData", "accessToken : " + accessToken);
-                            Log.i("LoginData", "refreshToken : " + refreshToken);
-                            Log.i("LoginData", "expiresAt : " + expiresAt);
-                            Log.i("LoginData", "tokenType : " + tokenType);
-                        } else {
-                            String errorCode = mOAuthLoginModule
-                                    .getLastErrorCode(mContext).getCode();
-                            String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
-                            Toast.makeText(mContext, "errorCode:" + errorCode
-                                    + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    ;
-                };
-                mOAuthLoginModule.startOauthLoginActivity(Login.this, mOAuthLoginHandler);
-            }
-
-
-        });
 
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -139,10 +153,12 @@ public class Login extends AppCompatActivity {
         updateKakaoLoginUi();
 
 
-
+        mID = (EditText) findViewById(R.id.ID);
+        mPW = (EditText) findViewById(R.id.PASSWORD);
 
         join = (Button) findViewById(R.id.Join);
         home = (Button) findViewById(R.id.home);
+        owner = (Button) findViewById(R.id.owner);
 
         join.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -159,6 +175,21 @@ public class Login extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        owner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*mPW.setVisibility(View.VISIBLE);
+                mID.setVisibility(View.VISIBLE);
+                join.setVisibility(View.GONE);
+                home.setVisibility(View.GONE);
+                nothing.setVisibility(View.GONE);
+                loginButton.setVisibility(View.GONE);*/
+                Intent i = new Intent(Login.this, Login_detail.class);
+                startActivity(i);
+            }
+        });
+
+
 
     }
 
@@ -166,34 +197,38 @@ public class Login extends AppCompatActivity {
         UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
             @Override
             public Unit invoke(User user, Throwable throwable) {
+                if(user != null){
+                    Log.i(TAG,"invoke: id=" + user.getId());
+                    long num = user.getId();
+                    String str = String.valueOf(num);
+                   if(str.equals("1827975262")) {
+                       Intent i = new Intent(Login.this, Login_detail2.class);
+                       startActivity(i);
+                   }
+                    Log.i(TAG,"invoke: email=" + user.getKakaoAccount().getEmail());
 
-                    if (user != null) {
-                        Log.i(TAG, "invoke: id=" + user.getId());
-                        Log.i(TAG, "invoke: email=" + user.getKakaoAccount().getEmail());
-                        Log.i(TAG, "invoke: nickname=" + user.getKakaoAccount().getProfile().getNickname());
-                        Log.i(TAG, "invoke: gender=" + user.getKakaoAccount().getGender());
-                        Log.i(TAG, "invoke: age=" + user.getKakaoAccount().getAgeRange());
-
-                        nickName.setText(user.getKakaoAccount().getProfile().getNickname());
-                        Glide.with(profileImage).load(user.getKakaoAccount().getProfile().getThumbnailImageUrl()).circleCrop().into(profileImage);
-                        loginButton.setVisibility(View.GONE);
-                        logoutButton.setVisibility(View.VISIBLE);
-
-
-                    } else {
-                        nickName.setText(null);
-                        profileImage.setImageBitmap(null);
-                        loginButton.setVisibility(View.VISIBLE);
-                        logoutButton.setVisibility(View.GONE);
-
-                    }
-                    return null;
+                    Log.i(TAG,"invoke: nickname=" + user.getKakaoAccount().getProfile().getNickname());
+                    Log.i(TAG,"invoke: gender=" + user.getKakaoAccount().getGender());
+                    Log.i(TAG,"invoke: age=" + user.getKakaoAccount().getAgeRange());
 
 
+                   Glide.with(profileImage).load(user.getKakaoAccount().getProfile().getThumbnailImageUrl()).into(profileImage);
+                    loginButton.setVisibility(View.GONE);
+                    logoutButton.setVisibility(View.VISIBLE);
+                    mID.setVisibility(View.GONE);
+                    mPW.setVisibility(View.GONE);
+
+
+                } else{
+
+                    profileImage.setImageBitmap(null);
+                    loginButton.setVisibility(View.VISIBLE);
+                    logoutButton.setVisibility(View.GONE);
+
+                }
+                return null;
             }
-
         });
     }
-
 
 }
---
+++ app/src/main/java/com/daelim/sauce/Login_detail3.java
@@ -0,0 +1,14 @@
+package com.daelim.sauce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_detail3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_detail3);
+    }
+}
